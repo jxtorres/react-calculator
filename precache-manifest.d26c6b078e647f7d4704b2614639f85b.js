fc648f187c7be7565bd61ad1070a16b1c88cec1b
Jordan <jxtbronco@gmail.com>
2022-08-15 22:05:06 -0500

    Updates

---
+++ precache-manifest.d26c6b078e647f7d4704b2614639f85b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5e88533991737b70c57e7e489b2f36",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "0559de6134b22bd35dce",
+    "url": "/calculator/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "1935197a70d965c11c9a",
+    "url": "/calculator/static/css/main.f89f2e89.chunk.css"
+  },
+  {
+    "revision": "0559de6134b22bd35dce",
+    "url": "/calculator/static/js/2.e35843d7.chunk.js"
+  },
+  {
+    "revision": "1935197a70d965c11c9a",
+    "url": "/calculator/static/js/main.852abe99.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
