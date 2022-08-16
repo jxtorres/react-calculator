794e5cf7756998dbc958ac5b09bc42df9d9eb9dc
jordan <jordantorres@utexas.edu>
2022-08-15 20:21:09 -0500

    Adopt Game Elements

--- src/component/App.js
+++ src/component/App.js
@@ -1,24 +1,51 @@
 import React from "react";
 import Display from "./Display";
+import GameDisplay from "./GameDisplay";
 import ButtonPanel from "./ButtonPanel";
-import calculate from "../logic/calculate";
+import calculate from "../logic/gameCalculate";
 import "./App.css";
+import IosAlert from "./IosAlert";
 
 export default class App extends React.Component {
   state = {
     total: null,
     next: null,
     operation: null,
+    isDisplayingError: false,
   };
 
+
   handleClick = buttonName => {
+    this.state.clicks = this.state.clicks ? this.state.clicks + 1: 1;
+    if(buttonName == "AC"){
+      this.state.clicks = 0;
+      this.state.mostRecentMessage = "";
+    }
     this.setState(calculate(this.state, buttonName));
+    //Calling setState will not actually modify the state or run the caclulcate method pointer
+    //State will be modified later in the render loop before render() is invoked.
+    //Render Loop will iterate at some time in near future.
+   
   };
 
   render() {
+    // Special Render State on a value of zero.
+    if(this.state.next == 0 && this.state.mostRecentMessage==""){
+      this.state.isDisplayingError = true;
+      this.state.mostRecentMessage = "Congrats on finishing in " + this.state.clicks + " clicks!"
+    }
+    //Set Flags for Alert rendering
+    let renderAlert = this.state.isDisplayingError;
+
+    //Clear one-time use flags
+    this.state.isDisplayingError = false;
+   
+    
     return (
       <div className="component-app">
-        <Display value={this.state.next || this.state.total || "0"} />
+        
+        <IosAlert message={this.state.mostRecentMessage} hidden={(renderAlert == false) ? true : false} />
+        <GameDisplay value={this.state.next || this.state.total || "0"} />
         <ButtonPanel clickHandler={this.handleClick} />
       </div>
     );
